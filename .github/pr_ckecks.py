fa492934c7d4e3f36da9f37b321c983d2c708ab9
Mirco Bianchini <57708659+sonomirco@users.noreply.github.com>
2023-01-04 13:59:52 +1100

    Create pr_ckecks.py

---
+++ .github/pr_ckecks.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import re
+import distutils.util
+from github import Github
+
+def read_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Create player id dataframe")
+    # we will parse the list as a string.
+    parser.add_argument("--token", type=str, help="The GitHub token")
+    
+    args = parser.parse_args()
+    return args
+  
+def bump_versions():
+    args = read_args()
+    print(f"inputs -> {args.token} - type -> {type(args.token)}")
